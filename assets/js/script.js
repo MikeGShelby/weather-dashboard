6137f4c009da6235d60ac108e961cd9dae2f3533
Mike Shelby <mikegshelby@gmail.com>
2020-11-01 15:10:20 -0600

    add JS functions for displaying UV index indicator colors and ability to save list of searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,104 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-input");
 
+var searchListEl = document.querySelector("#city-search-list");
+var cityBtnEl = document.querySelector(".city-btn");
+
 var overviewContainerEl = document.querySelector("#weather-overview");
-var forecastContainerEl = document.querySelector("#forecast-cards");
+
 var forecastSectionEl = document.querySelector(".forecast");
 var forecastTitleEl = document.querySelector(".forecast-title");
+var forecastContainerEl = document.querySelector("#forecast-cards");
+
+// empty array to store all search inputs
+var searchHistory = []
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
     // get value from input element
     var city = cityInputEl.value.trim();
-    console.log(city);
 
+    //if a city name is entered, run functions. Otherwise, prompt user to enter a city name
     if (city) {
-    getCityCoords(city);
-    cityInputEl.value = "";
+        // run function to get city latitude and longitude data
+        getCityCoords(city);
+        cityInputEl.value = "";
+
+        // run function to add city input to search list
+        createSearchList(city);
     }
     else {
     alert("Please enter a city name");
     }
 };
+
+var buttonClickHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var city = event.target.getAttribute("data-city");
+    console.log(city);
+
+    // if a city name is entered, run functions. Otherwise, prompt user to enter a city name
+    if (city) {
+        // run function to get city latitude and longitude data
+        getCityCoords(city);
+        cityInputEl.value = "";
+
+        // run function to add city input to search list
+        createSearchList(city);
+    }
+    else {
+    alert("Please enter a city name");
+    }
+};
+
+var createSearchList = function(city) {
+    // clear search list
+    searchListEl.innerHTML = null;
+
+    // add search input to start of search history list
+    searchHistory.unshift(city);
+
+    // create search history array with no duplicate values
+    uniqueSearchHistory = Array.from(new Set(searchHistory));
+    console.log(uniqueSearchHistory);
+
+    for (i = 0; i < uniqueSearchHistory.length; i++) {
+
+        // create list item elements
+        listItemEl = document.createElement("li");
+
+        // create button elements
+        searchHistoryBtnEl = document.createElement("button");
+        searchHistoryBtnEl.setAttribute("class", "btn city-btn");
+        searchHistoryBtnEl.setAttribute("data-city", uniqueSearchHistory[i]);
+        searchHistoryBtnEl.textContent = uniqueSearchHistory[i];
+        listItemEl.appendChild(searchHistoryBtnEl);
+
+        searchListEl.appendChild(listItemEl);
+    }
+
+    saveSearchList(uniqueSearchHistory);
+}
+
+
+var saveSearchList = function(uniqueSearchHistory) {
+    localStorage.setItem("cities", JSON.stringify(uniqueSearchHistory));
+
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    console.log(storedCities);
+}
+
+// var loadSearchList = function() {
+//     var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+//     for (i = 0; 0 < storedCities.length; i++) {
+//         createSearchList(storedCities[i]);
+//     }
+// }
+
 
 
 var getCityCoords = function(city) {
@@ -45,10 +123,6 @@ var getCityData = function(cityLat, cityLon, city) {
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
         console.log(data);
-        console.log("Temperature: " + data.daily[0].temp.day + " °F");
-        console.log("Humidity: " + data.daily[0].humidity + "%");
-        console.log("Wind Speed: " + data.daily[0].wind_speed + " MPH");
-        console.log("UV Index: " + data.daily[0].uvi);
 
         displayOverview(data, city);
         });
@@ -84,7 +158,6 @@ var displayOverview = function(data, city) {
     overviewTitleIconEl.setAttribute("class", "overview-icon");
     overviewTitleEl.appendChild(overviewTitleIconEl);
 
-
     // Create and append a p element for temperature
     var temperature = "Temperature: " + data.daily[0].temp.day + " °F";
     temperatureEl = document.createElement("p");
@@ -104,13 +177,23 @@ var displayOverview = function(data, city) {
     overviewContainerEl.appendChild(windSpeedEl);
 
     // Create and append a p element for UV Index
-    var uvIndex = "UV Index: " + data.daily[0].uvi;
+    var uvIndex = data.daily[0].uvi;
     uvIndexEl = document.createElement("p");
-    uvIndexEl.innerText = uvIndex;
+
+    // evaluate and style uv index
+    if (uvIndex <= 2) {
+        uvIndexEl.innerHTML = "UV Index: <span class='uvi uvi-low'>" + uvIndex + "</span>";
+    }
+    else if (uvIndex > 2 && uvIndex <= 6) {
+        uvIndexEl.innerHTML = "UV Index: <span class='uvi uvi-moderate'>" + uvIndex + "</span>";
+    }
+    else if (uvIndex > 6) {
+        uvIndexEl.innerHTML = "UV Index: <span class='uvi uvi-high'>" + uvIndex + "</span>";
+    }
+
     overviewContainerEl.appendChild(uvIndexEl);
 
     displayForecast(data);
-
 }
 
 var displayForecast = function(data) {
@@ -120,7 +203,6 @@ var displayForecast = function(data) {
     // add forecast section title
     forecastTitleEl.textContent = "5-Day Forecast";
 
-
     // Create and display forecast cards for each of the five forecast days
     for (var i = 1; i < 6; i++) {
 
@@ -128,15 +210,12 @@ var displayForecast = function(data) {
         var forecastCardEl = document.createElement("div");
         forecastCardEl.setAttribute("class", "forecast-card");
 
-        // Create data element for each forecast day
+        // Create date element for each forecast day
         var currentDate = new Date();
-        console.log(currentDate);
         var month = currentDate.getMonth()+1;
         var day = currentDate.getDate() + parseInt([i]);
-        console.log(day);
         var year = currentDate.getFullYear();
         var fullDate = month + "/" + day + "/" + year;
-        console.log(fullDate);
 
         dateEl = document.createElement("h4");
         dateEl.textContent = fullDate;
@@ -162,42 +241,12 @@ var displayForecast = function(data) {
         humidityEl.innerText = humidity;
         forecastCardEl.appendChild(humidityEl);
 
-
-
-
         forecastContainerEl.appendChild(forecastCardEl);
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    // // Create and append a p element for wind speed
-    // var windSpeed = "Wind Speed: " + data.daily[0].wind_speed + " MPH";
-    // windSpeedEl = document.createElement("p");
-    // windSpeedEl.innerText = windSpeed;
-    // overviewContainerEl.appendChild(windSpeedEl);
-
-    // // Create and append a p element for UV Index
-    // var uvIndex = "UV Index: " + data.daily[0].uvi;
-    // uvIndexEl = document.createElement("p");
-    // uvIndexEl.innerText = uvIndex;
-    // overviewContainerEl.appendChild(uvIndexEl);
-
 }
 
 
-
 searchFormEl.addEventListener("submit", formSubmitHandler);
+searchListEl.addEventListener("click", buttonClickHandler);
 
-// https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=f1861af816f9e98e7d029ccebf696d61
-
-// https://api.openweathermap.org/data/2.5/onecall?lat=47.61&lon=-122.33&appid=f1861af816f9e98e7d029ccebf696d61
+// loadSearchList();
